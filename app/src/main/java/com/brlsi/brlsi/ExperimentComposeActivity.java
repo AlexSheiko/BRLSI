9bfb4416291e29c420145fb3cdd04031c8c3f79e
Aleksey Sheyko <aleksey_sheyko@hotmail.com>
2015-09-11 21:45:12 +0300

    Allow editing existing experiment

--- app/src/main/java/com/brlsi/brlsi/ExperimentComposeActivity.java
+++ app/src/main/java/com/brlsi/brlsi/ExperimentComposeActivity.java
@@ -1,10 +1,8 @@
 package com.brlsi.brlsi;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -48,8 +46,7 @@ public class ExperimentComposeActivity extends AppCompatActivity {
 
     @Bind(R.id.finishButton)
     Button finishButton;
-
-    private SharedPreferences mPrefs;
+    private ParseObject mExperiment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,11 +54,7 @@ public class ExperimentComposeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_compose_experiment);
         ButterKnife.bind(this);
 
-        mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-
-//        TODO: Add back button arrow
-//        TODO: Set input types for all fields
-//        TODO: Insert photo icon
+        // TODO: Insert photo icon
 
 
         Typeface signika = Typeface.createFromAsset(getAssets(), "Signika-Bold.otf");
@@ -74,6 +67,7 @@ public class ExperimentComposeActivity extends AppCompatActivity {
             query.getInBackground(experimentId, new GetCallback<ParseObject>() {
                 @Override
                 public void done(ParseObject experiment, ParseException e) {
+                    mExperiment = experiment;
                     populateFields(experiment);
                 }
             });
@@ -97,7 +91,7 @@ public class ExperimentComposeActivity extends AppCompatActivity {
         if (name != null) {
             nameField.setText(name);
         }
-        if (title != null) {
+        if (title != null && !title.equals("No title")) {
             titleField.setText(title);
         }
         if (date != null) {
@@ -132,22 +126,8 @@ public class ExperimentComposeActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        restoreFieldValues();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        saveFieldValues();
-    }
-
     public void finishExperiment(View view) {
-        saveFieldValues();
         saveExperiment();
-        clearFieldValues();
 
         Intent returnIntent = new Intent();
         setResult(RESULT_OK, returnIntent);
@@ -168,7 +148,19 @@ public class ExperimentComposeActivity extends AppCompatActivity {
         String whyDidHappen = whyDidHappenField.getText().toString();
         String howToUse = howToUseField.getText().toString();
 
-        ParseObject experiment = new ParseObject("Experiment");
+        if (title.isEmpty()) {
+            title = "No title";
+            if (!date.isEmpty()) {
+                title += " " + date;
+            }
+        }
+
+        ParseObject experiment;
+        if (mExperiment == null) {
+            experiment = new ParseObject("Experiment");
+        } else {
+            experiment = mExperiment;
+        }
         experiment.put("author", ParseUser.getCurrentUser());
         experiment.put("name", name);
         experiment.put("title", title);
@@ -185,77 +177,9 @@ public class ExperimentComposeActivity extends AppCompatActivity {
         experiment.saveEventually();
     }
 
-    private void saveFieldValues() {
-        String name = nameField.getText().toString();
-        String title = titleField.getText().toString();
-        String date = dateField.getText().toString();
-        String time = timeField.getText().toString();
-        String location = locationField.getText().toString();
-        String conditions = conditionsField.getText().toString();
-        String equipment = equipmentField.getText().toString();
-        String action = actionField.getText().toString();
-        String outcome = outcomeField.getText().toString();
-        String whatHappened = whatHappenedField.getText().toString();
-        String whyDidHappen = whyDidHappenField.getText().toString();
-        String howToUse = howToUseField.getText().toString();
-
-        mPrefs.edit()
-                .putString("name", name)
-                .putString("title", title)
-                .putString("date", date)
-                .putString("time", time)
-                .putString("location", location)
-                .putString("conditions", conditions)
-                .putString("equipment", equipment)
-                .putString("action", action)
-                .putString("outcome", outcome)
-                .putString("whatHappened", whatHappened)
-                .putString("whyDidHappen", whyDidHappen)
-                .putString("howToUse", howToUse)
-                .apply();
-    }
-
-    private void restoreFieldValues() {
-        String name = mPrefs.getString("name", ParseUser.getCurrentUser().getUsername());
-        String title = mPrefs.getString("title", "");
-        String date = mPrefs.getString("date", "");
-        String time = mPrefs.getString("time", "");
-        String location = mPrefs.getString("location", "");
-        String conditions = mPrefs.getString("conditions", "");
-        String equipment = mPrefs.getString("equipment", "");
-        String action = mPrefs.getString("action", "");
-        String outcome = mPrefs.getString("outcome", "");
-        String whatHappened = mPrefs.getString("whatHappened", "");
-        String whyDidHappen = mPrefs.getString("whyDidHappen", "");
-        String howToUse = mPrefs.getString("howToUse", "");
-
-        nameField.setText(name);
-        titleField.setText(title);
-        dateField.setText(date);
-        timeField.setText(time);
-        locationField.setText(location);
-        conditionsField.setText(conditions);
-        equipmentField.setText(equipment);
-        actionField.setText(action);
-        outcomeField.setText(outcome);
-        whatHappenedField.setText(whatHappened);
-        whyDidHappenField.setText(whyDidHappen);
-        howToUseField.setText(howToUse);
-    }
-
-    private void clearFieldValues() {
-        nameField.setText("");
-        titleField.setText("");
-        dateField.setText("");
-        timeField.setText("");
-        locationField.setText("");
-        conditionsField.setText("");
-        equipmentField.setText("");
-        actionField.setText("");
-        outcomeField.setText("");
-        whatHappenedField.setText("");
-        whyDidHappenField.setText("");
-        howToUseField.setText("");
-        mPrefs.edit().clear().apply();
+    @Override
+    public void onBackPressed() {
+        finishExperiment(new View(this));
+        super.onBackPressed();
     }
 }

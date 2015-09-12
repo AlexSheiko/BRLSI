c772e12d4d2f16a84e7bbf874010e901fe659406
Aleksey Sheyko <aleksey_sheyko@hotmail.com>
2015-09-12 13:04:28 +0300

    Add first take photo button

--- app/src/main/java/com/brlsi/brlsi/ExperimentComposeActivity.java
+++ app/src/main/java/com/brlsi/brlsi/ExperimentComposeActivity.java
@@ -4,23 +4,32 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -56,6 +65,9 @@ public class ExperimentComposeActivity extends AppCompatActivity
     @Bind(R.id.finishButton)
     Button finishButton;
     private ParseObject mExperiment;
+
+    private static final int REQUEST_IMAGE_CAPTURE = 101;
+    String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -118,7 +130,7 @@ public class ExperimentComposeActivity extends AppCompatActivity
             @Override
             public boolean onTouch(View inputField, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_UP) {
-                    if (((EditText)inputField).getText().toString().isEmpty()) {
+                    if (((EditText) inputField).getText().toString().isEmpty()) {
                         new TimePickerDialog(
                                 ExperimentComposeActivity.this,
                                 ExperimentComposeActivity.this,
@@ -275,5 +287,57 @@ public class ExperimentComposeActivity extends AppCompatActivity
         String date = dateFormat.format(calendar.getTime());
         dateField.setText(date);
         dateField.setSelection(dateField.length());
+    }
+
+    public void attachPhoto(View view) {
+        dispatchTakePictureIntent();
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ignored) {
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+            }
+        } else {
+            Toast.makeText(this, "Install camera app to take a picture", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            ImageButton cameraButton = (ImageButton) findViewById(R.id.camera_conditions);
+            cameraButton.setAlpha(1f);
+            Glide.with(this).load(mCurrentPhotoPath).fitCenter().centerCrop().into(cameraButton);
+        }
     }
 }
